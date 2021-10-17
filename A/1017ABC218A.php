92a44c3c0d9b2742bf38435d3bb05bc44a64aaea
kisanasaki <saki.virtualreality@gmail.com>
2021-10-17 19:09:34 +0900

    1017ABC218Aできた

--- A/1017ABC218A.php
+++ A/1017ABC218A.php
@@ -1,3 +1,12 @@
 <?php
+$N = fgets(STDIN);
+$S = fgets(STDIN);
+// 一文字ずつ分解
+$string = str_split($S);
+if($string[$N-1] == 'o'){
+    echo 'Yes';
+}else if($string[$N-1] == 'x'){
+    echo 'No';
+}
 
 ?>
