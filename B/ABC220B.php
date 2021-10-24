244e505df48597d0fc3a3832275d3e66b2a2080f
kisanasaki <saki.virtualreality@gmail.com>
2021-10-24 22:47:30 +0900

    ABC220Bできた

--- B/ABC220B.php
+++ B/ABC220B.php
@@ -1,28 +1,23 @@
 <?php
 $K = fgets(STDIN);
 fscanf(STDIN,"%d %d",$A,$B);
-
 $BaseK = new BaseK;
-
 $AA = $BaseK->trans($A,$K);
 $BB = $BaseK->trans($B,$K);
 $ANS = $BaseK->multiplication($AA,$BB);
-
-//echo $BB;
-
+echo $ANS;
 class BaseK {
     public static function trans($x,$y){
         $arr = str_split($x);
         $n = count($arr);
-        //echo $n;
+        $answer = 0;
         for($i = 0;$i<$n;$i++){
-            $array = array_pop($arr);
-            echo $array;
-            $ans =+(int)$array*pow($y,$i);
+            $array[] = array_pop($arr);
+            $ans =(int)$array[$i]*pow($y,$i);
+            $answer = $answer +(int)$ans;
         }
-        return $ans;
+        return $answer;
     }
-
     public static function multiplication($x,$y){
         $ans = $x * $y;
         return $ans;
